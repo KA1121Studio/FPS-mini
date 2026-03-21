1a41a4dff955c7d3aeffba45a3411bf00d849cf5
K.A <akkazu1121@gmail.com>
2026-03-21 22:41:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -61,9 +61,14 @@ const createScene = () => {
 
   // 回転（これが本体）
 gun.rotation = new BABYLON.Vector3(
- Math.PI / 2, 
--Math.PI / 2,
+  Math.PI / 2,
+  -Math.PI / 2,
+  0
 );
+
+// ←ここ追加（重要）
+gun.rotation.y += Math.PI / 2;
+
    
   // 透明バグ修正
   gun.getChildMeshes().forEach(mesh => {
